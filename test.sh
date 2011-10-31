a9cb1f6f38f685d85f58ba35b080987fad58d735
janecjak <janecjak@fit.cvut.cz>
2011-10-31 09:59:14 +0100

    New version of Knapsack test file with file IO and program entry point - grammar changed accordingly.

--- test.sh
+++ test.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 cd out
+
 echo "test01.claus"
 java Claus < ../test/test01.claus
 echo ""
+
 echo "Knapsack01.claus"
 java Claus < ../test/Knapsack01.claus
+echo ""
+
+echo "Knapsack02.claus"
+java Claus < ../test/Knapsack02.claus
 echo ""
