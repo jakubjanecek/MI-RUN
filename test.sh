b983d4e8be3f2e6a10e8ddbafb27f8a13005ddaa
janecjak <janecjak@fit.cvut.cz>
2011-11-02 17:00:04 +0100

    New tests.

--- test.sh
+++ test.sh
@@ -6,6 +6,22 @@ echo "test01.claus"
 java Claus < ../test/test01.claus
 echo ""
 
+echo "test02.claus"
+java Claus < ../test/test02.claus
+echo ""
+
+echo "test03.claus"
+java Claus < ../test/test03.claus
+echo ""
+
+echo "test04.claus"
+java Claus < ../test/test04.claus
+echo ""
+
+echo "test05.claus"
+java Claus < ../test/test05.claus
+echo ""
+
 echo "Knapsack01.claus"
 java Claus < ../test/Knapsack01.claus
 echo ""
