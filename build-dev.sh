d3e18e31062afef466fc4157577e6aed35ecc4cd
drkulond <drkulond@fit.cvut.cz>
2011-12-17 18:57:38 +0100

    Pridany importy potrebnych trid do parseru.

--- build-dev.sh
+++ build-dev.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 rm -rf out/*
-./javacc/bin/jjtree -NOSTATIC -OUTPUT_DIRECTORY=out src/dev/grammar-claus.jjt
+./javacc/bin/jjtree -NOSTATIC -OUTPUT_DIRECTORY=out src/grammar-claus.jjt
 ./javacc/bin/javacc -NOSTATIC -OUTPUT_DIRECTORY=out out/grammar-claus.jj
 javac out/*.java
