3f61c96ffc9dba1985b42a07b72f74e1cbffa88d
janecjak <janecjak@fit.cvut.cz>
2011-12-17 20:49:06 +0100

    build-dev.sh

--- build-dev.sh
+++ build-dev.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 rm -rf out/*
+cp -R src/vm out
 ./javacc/bin/jjtree -NOSTATIC -OUTPUT_DIRECTORY=out src/grammar-claus.jjt
 ./javacc/bin/javacc -NOSTATIC -OUTPUT_DIRECTORY=out out/grammar-claus.jj
-javac out/*.java
+javac out/vm/mm/*.java out/vm/*.java out/*.java
