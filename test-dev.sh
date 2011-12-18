383468ffa38385c1e30e86a691d73c87a9029419
drkulond <drkulond@fit.cvut.cz>
2011-12-18 13:12:15 +0100

    Upravy parseru.

--- test-dev.sh
+++ test-dev.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
 cd out
-echo "-------------------"
+echo ""
+echo "---------------------------------------------------------"
+echo ""
 echo "[FirstTest]:"
+echo ""
 java Claus < ../test/FirstTest.claus
-echo "-------------------"
-echo "[Knapsack03]:"
-java Claus < ../test/Knapsack03.claus
-echo "-------------------"
+echo ""
+echo "---------------------------------------------------------"
+#echo ""
+#echo "[Knapsack03]:"
+#echo ""
+#java Claus < ../test/Knapsack03.claus
+#echo ""
+#echo "---------------------------------------------------------"
 cd ..
