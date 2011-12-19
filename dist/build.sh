3fc7a8086b08b56cadd5dae5edc84e1dc6cd0420
drkulond <drkulond@fit.cvut.cz>
2011-12-19 06:10:13 +0100

    Pridany vychozi skripty do slozky 'dist'.

---
+++ dist/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd ..
+rm -rf out/*
+cp -R src/vm out
+./javacc/bin/jjtree -NOSTATIC -OUTPUT_DIRECTORY=out src/grammar-claus.jjt
+./javacc/bin/javacc -NOSTATIC -OUTPUT_DIRECTORY=out out/grammar-claus.jj
+javac out/vm/mm/*.java out/vm/*.java out/*.java
+cd ./dist
---
+++ dist/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd ../out
+echo ""
+java Claus < ../test/FirstTest.claus
+echo ""
+cd ../dist
