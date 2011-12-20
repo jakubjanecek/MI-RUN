b4ba678d8001e9dd6c8ceac73aee04bcad63299f
drkulond <drkulond@fit.cvut.cz>
2011-12-20 17:24:18 +0100

    .

--- build-dev.sh
+++ build-dev.sh
@@ -2,6 +2,6 @@
 
 rm -rf out/*
 cp -R src/vm out
-./javacc/bin/jjtree -NOSTATIC -OUTPUT_DIRECTORY=out src/grammar-claus.jjt
-./javacc/bin/javacc -NOSTATIC -OUTPUT_DIRECTORY=out out/grammar-claus.jj
+./javacc/bin/jjtree -NOSTATIC -OUTPUT_DIRECTORY=out src/claus_parser.jj
+./javacc/bin/javacc -NOSTATIC -OUTPUT_DIRECTORY=out out/claus_parser.jj
 javac out/vm/mm/*.java out/vm/*.java out/*.java
