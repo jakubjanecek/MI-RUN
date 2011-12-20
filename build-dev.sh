8555dd617c5e2f4505f34b97b70b298ba3089cf9
drkulond <drkulond@fit.cvut.cz>
2011-12-20 21:18:48 +0100

    Uprava buildovacich skriptu.

--- build-dev.sh
+++ build-dev.sh
@@ -2,6 +2,6 @@
 
 rm -rf out/*
 cp -R src/vm out
-./javacc/bin/jjtree -NOSTATIC -OUTPUT_DIRECTORY=out src/claus_parser.jj
+./javacc/bin/jjtree -NOSTATIC -OUTPUT_DIRECTORY=out src/claus_parser.jjt
 ./javacc/bin/javacc -NOSTATIC -OUTPUT_DIRECTORY=out out/claus_parser.jj
 javac out/vm/mm/*.java out/vm/*.java out/*.java
