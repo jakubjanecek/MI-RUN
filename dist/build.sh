42f630d0b03c3816f23bc050727cee8cce4c580e
drkulond <drkulond@fit.cvut.cz>
2011-12-20 17:24:40 +0100

    .

--- dist/build.sh
+++ dist/build.sh
@@ -4,7 +4,7 @@ rm -rf out/*
 cp -R src/vm out
 cp -R test/vm out
 cp test/input.dat out/input.dat
-./javacc/bin/jjtree -NOSTATIC -OUTPUT_DIRECTORY=out src/grammar-claus.jjt
-./javacc/bin/javacc -NOSTATIC -OUTPUT_DIRECTORY=out out/grammar-claus.jj
+./javacc/bin/jjtree -NOSTATIC -OUTPUT_DIRECTORY=out src/claus_parser.jj
+./javacc/bin/javacc -NOSTATIC -OUTPUT_DIRECTORY=out out/claus_parser.jj
 
 javac -cp .:junit-4.10.jar:junit-dep-4.10.jar out/vm/mm/*.java out/vm/*.java out/*.java
